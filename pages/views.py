0282e9e462c90a2ce0cac726cfef0c6885adad24
Gavin <gavinwaynebrown@gmail.com>
2024-09-19 11:31:12 -0600

    initial commit

---
+++ pages/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home_page_view(request):
+    return HttpResponse("Homepage")
+
+def about_page_view(request):
+    context = {"name": "Gavin",
+               "age": 33,
+               }
+    return render(request, "pages/about.html", context)
